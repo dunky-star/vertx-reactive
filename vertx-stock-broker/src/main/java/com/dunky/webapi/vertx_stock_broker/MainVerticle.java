23bed70569658366181d1a58967e162cde0fc6b4
dunky-star <dunkygeoffrey39@gmail.com>
2024-11-03 08:27:13 -0700

    vertx web api

---
+++ vertx-stock-broker/src/main/java/com/dunky/webapi/vertx_stock_broker/MainVerticle.java
@@ -0,0 +1,43 @@
+package com.dunky.webapi.vertx_stock_broker;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainVerticle extends AbstractVerticle {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+
+  public static void main(String[] args) {
+    var vertx = Vertx.vertx();
+    vertx.exceptionHandler(error -> {
+      LOG.error("Unhandled: {} ", error);
+    });
+
+    vertx.deployVerticle(new MainVerticle(), ar -> {
+      if(ar.failed()){
+        LOG.error("Falied to deploy: {}", ar.cause() );
+        return;
+      }
+      LOG.info("Deployed MainVerticle");
+    });
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello from Vert.x!");
+    }).listen(8888).onComplete(http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8888");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+}
