87125d40a8a29dcdd8d890db75941d0d147ac17c
dunky-star <dunkygeoffrey39@gmail.com>
2024-11-03 08:52:14 -0700

    Configuring logback for slf4j logging

--- vertx-stock-broker/src/main/java/com/dunky/webapi/vertx_stock_broker/MainVerticle.java
+++ vertx-stock-broker/src/main/java/com/dunky/webapi/vertx_stock_broker/MainVerticle.java
@@ -3,6 +3,9 @@ package com.dunky.webapi.vertx_stock_broker;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,14 +30,29 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888).onComplete(http -> {
+    // Router
+    final Router restApi = Router.router(vertx);
+
+    // HTTP end-point and request handler
+    restApi.get("/assets").handler(context -> {
+      final JsonArray response = new JsonArray();
+      response
+        .add(new JsonObject().put("symbol", "AAPL"))
+        .add(new JsonObject().put("symbol", "AMZN"))
+        .add(new JsonObject().put("symbol", "NFLX"))
+        .add(new JsonObject().put("symbol", "TSLA"));
+
+      LOG.info("Path {} responds with {}", context.normalizedPath(), response.encode());
+      context.response().end(response.toBuffer());
+    });
+
+    vertx.createHttpServer()
+      .requestHandler(restApi)
+        .exceptionHandler(error -> LOG.error("HTTP Server error: ", error))
+      .listen(8888).onComplete(http -> {
       if (http.succeeded()) {
         startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
+        LOG.info("HTTP server started on port 8888");
       } else {
         startPromise.fail(http.cause());
       }
