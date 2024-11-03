ce72ae851387f762fc238e16e2894bae55cf176e
dunky-star <dunkygeoffrey39@gmail.com>
2024-11-02 22:20:44 -0700

    Sending JSON Object over the event bus

--- vertex-starter/src/main/java/com/dunky/vertex/vertex_starter/MainVerticle.java
+++ vertex-starter/src/main/java/com/dunky/vertex/vertex_starter/MainVerticle.java
@@ -3,12 +3,14 @@ package com.dunky.vertex.vertex_starter;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class MainVerticle extends AbstractVerticle {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
 
   public static void main(String[] args) {
     var vertx = Vertx.vertx();
@@ -24,7 +26,7 @@ public class MainVerticle extends AbstractVerticle {
     }).listen(8888).onComplete(http -> {
       if (http.succeeded()) {
         startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
+        LOG.info("HTTP server started on port 8888");
       } else {
         startPromise.fail(http.cause());
       }
